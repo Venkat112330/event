fbad1f4b205ddc46e2e228c77415b96554eb0373
Venkat Donakonda <venkatdonakonda@Macbook.local>
2024-09-05 22:13:08 +0530

    frontend

---
+++ src/utils/api.js
@@ -0,0 +1,5 @@
+// const BASE_URL = "http://localhost:4000";
+const BASE_URL = "https://backend-oor-bb.vercel.app"
+
+const IMAGE_URL = "https://res.cloudinary.com/drfvhp1jh/image/upload";
+module.exports = { BASE_URL, IMAGE_URL };
